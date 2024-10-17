9bb3972c057cd1bef65ed350fee60576a158f78e
13ruceYu <bruceyuhongbo@gmail.com>
2024-10-17 15:11:28 +0800

    project init

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,8 @@
 <script setup lang="ts">
-definePageMeta({
-  layout: 'home',
-})
-
-const online = useOnline()
 </script>
 
 <template>
   <div>
-    <Suspense>
-      <ClientOnly>
-        <PageView v-if="online" />
-        <div v-else text-gray:80>
-          You're offline
-        </div>
-      </ClientOnly>
-      <template #fallback>
-        <div italic op50>
-          <span animate-pulse>Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-    <InputEntry />
+    hi world
   </div>
 </template>
